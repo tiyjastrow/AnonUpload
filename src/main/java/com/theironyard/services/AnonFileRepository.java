1bb8fd57ac0e94d8b793495929794ce3c5cf9f2d
Zach Benton <Zach@Zach.local>
2016-10-14 11:34:18 -0500

    working

---
+++ src/main/java/com/theironyard/services/AnonFileRepository.java
@@ -0,0 +1,13 @@
+package com.theironyard.services;
+
+import com.theironyard.entities.AnonFile;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Zach on 10/11/16.
+ */
+
+public interface AnonFileRepository extends CrudRepository<AnonFile, Integer>{
+
+
+}
